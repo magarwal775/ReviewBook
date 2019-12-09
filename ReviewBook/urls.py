0ca95328500b7311a7065fbc42825bfdbd37c69e
Chirag Agarwal <chiragagarwal54@yahoo.com>
2019-12-10 01:30:18 +0530

    Added login and logout views and forms

--- ReviewBook/urls.py
+++ ReviewBook/urls.py
@@ -4,10 +4,14 @@ from django.urls import path, include
 from base.views import (
     registration_view,
     base,
+    logout_view,
+    login_view,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', registration_view, name="register"),
     path('', base, name="home"),
+    path('logout/', logout_view, name="logout"),
+    path('login/', login_view, name="login"),
 ]
--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
 
 from base.models import Account
 
@@ -10,3 +11,19 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = Account
         fields = ("first_name", "last_name", "username", "email", "dob", "password1", "password2")
+
+
+class AccountAuthenticationForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ('email', 'password')
+
+    def clean(self):
+        if self.is_valid():
+            email=self.cleaned_data['email']
+            password=self.cleaned_data['password']
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError("Invalid Login")
