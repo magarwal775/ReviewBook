d2c156162d94e329d113427d7216d038fc4c616b
Chirag Agarwal <chiragagarwal54@yahoo.com>
2019-12-13 12:24:15 +0530

    Implemented model for review post, and some changes

--- ReviewBook/settings.py
+++ ReviewBook/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'games',
     'movies',
     'series',
+    'review',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
--- ReviewBook/urls.py
+++ ReviewBook/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from base.views import (
     base,
@@ -11,3 +13,7 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('base.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
